1397d3ac9b0772cc39e65c2a326c01ffa839011e
Simon Ellensohn <ellensohn.simon@gmail.com>
2024-03-11 22:57:01 +0100

    wip

--- functions.zsh
+++ functions.zsh
@@ -8,3 +8,12 @@ function weather() {
    eval "curl http://wttr.in/${CITY}"
 }
 
+function mysqls() {
+   MYSQL_PORT="$1"
+
+   if [ -z "$MYSQL_PORT" ]; then
+      MYSQL_PORT="3306"
+   fi
+
+   command mysql --socket /tmp/mysql_$MYSQL_PORT.sock -uroot
+}
--- path.zsh
+++ path.zsh
@@ -6,3 +6,5 @@ export PATH="node_modules/.bin:vendor/bin:$PATH"
 
 # Make sure coreutils are loaded before system commands
 export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+
+export PATH=/Users/Shared/DBngin/mysql/8.0.33/bin:$PATH
